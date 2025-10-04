fa6c1d8ae989fd3faa67dc477223b26e186c4c59
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2025-10-04 21:24:37 +0300

    add app_assets

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,93 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAncient Egypt
+  /// assets/images/Ancient Egypt.png
+  static const String imagesAncientEgypt = "assets/images/Ancient Egypt.png";
+
+  /// Assets for imagesDetails1
+  /// assets/images/details1.svg
+  static const String imagesDetails1 = "assets/images/details1.svg";
+
+  /// Assets for imagesDetails2
+  /// assets/images/details2.svg
+  static const String imagesDetails2 = "assets/images/details2.svg";
+
+  /// Assets for imagesDetails3
+  /// assets/images/details3.svg
+  static const String imagesDetails3 = "assets/images/details3.svg";
+
+  /// Assets for imagesForgotPassword
+  /// assets/images/forgotPassword.png
+  static const String imagesForgotPassword = "assets/images/forgotPassword.png";
+
+  /// Assets for imagesHomeIcon
+  /// assets/images/home_icon.svg
+  static const String imagesHomeIcon = "assets/images/home_icon.svg";
+
+  /// Assets for imagesHomeIconActive
+  /// assets/images/home_icon_active.svg
+  static const String imagesHomeIconActive = "assets/images/home_icon_active.svg";
+
+  /// Assets for imagesIslamic Era
+  /// assets/images/Islamic Era.png
+  static const String imagesIslamicEra = "assets/images/Islamic Era.png";
+
+  /// Assets for imagesLionheart
+  /// assets/images/Lionheart.png
+  static const String imagesLionheart = "assets/images/Lionheart.png";
+
+  /// Assets for imagesMenu
+  /// assets/images/menu.svg
+  static const String imagesMenu = "assets/images/menu.svg";
+
+  /// Assets for imagesOnboadingThree
+  /// assets/images/onboading_three.png
+  static const String imagesOnboadingThree = "assets/images/onboading_three.png";
+
+  /// Assets for imagesOnboardingOne
+  /// assets/images/onboarding_one.png
+  static const String imagesOnboardingOne = "assets/images/onboarding_one.png";
+
+  /// Assets for imagesOnboardingTwo
+  /// assets/images/onboarding_two.png
+  static const String imagesOnboardingTwo = "assets/images/onboarding_two.png";
+
+  /// Assets for imagesPerson
+  /// assets/images/person.svg
+  static const String imagesPerson = "assets/images/person.svg";
+
+  /// Assets for imagesPersonActive
+  /// assets/images/person_active.svg
+  static const String imagesPersonActive = "assets/images/person_active.svg";
+
+  /// Assets for imagesSearch
+  /// assets/images/search.svg
+  static const String imagesSearch = "assets/images/search.svg";
+
+  /// Assets for imagesSearchActive
+  /// assets/images/search_active.svg
+  static const String imagesSearchActive = "assets/images/search_active.svg";
+
+  /// Assets for imagesShoppingCart
+  /// assets/images/shopping-cart.svg
+  static const String imagesShoppingCart = "assets/images/shopping-cart.svg";
+
+  /// Assets for imagesShoppingCartActive
+  /// assets/images/shopping-cart-active.svg
+  static const String imagesShoppingCartActive = "assets/images/shopping-cart-active.svg";
+
+  /// Assets for imagesSucccesOrder
+  /// assets/images/succcesOrder.png
+  static const String imagesSucccesOrder = "assets/images/succcesOrder.png";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/Vector2.svg
+  static const String imagesVector2 = "assets/images/Vector2.svg";
+}
+
