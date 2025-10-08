77cabcc562848296eb95f64227243f07b0fe77d3
Ahmed ElQady <ahmedzakaria3390@gmail.com>
2025-10-08 22:32:57 +0300

    create signup cubit and state with signupwith emailandpassword function

--- lib/features/auth/presentation/cubit/authsignup_cubit/auth_signup_cubit_state.dart
+++ lib/features/auth/presentation/cubit/authsignup_cubit/auth_signup_cubit_state.dart
@@ -1,6 +1,12 @@
-part of 'auth_signup_cubit.dart';
-
-@immutable
-sealed class AuthSignUpCubitState {}
+abstract class AuthSignUpCubitState {}
 
 final class AuthSignupCubitInitial extends AuthSignUpCubitState {}
+
+final class AuthSignupCubitLoadingState extends AuthSignUpCubitState {}
+
+final class AuthSignupCubitSuccessState extends AuthSignUpCubitState {}
+
+final class AuthSignupCubitFailureState extends AuthSignUpCubitState {
+  final String errorMessage;
+  AuthSignupCubitFailureState({required this.errorMessage});
+}
